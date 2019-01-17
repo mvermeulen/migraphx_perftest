8868811f2055a25827488f8a1e021790fb1c5ea7
Mike Vermeulen <mike@vermeulen.com>
2019-01-17 10:37:20 -0600

    Add basic run.

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@
 # Parameters - these can be modified by passing in as ENVIRONMENT variables.
 #              Default values are found to right of the := variable.
 
+env LD_LIBRARY_PATH=./lib ./bin/perf_onnx ./onnx/resnet50i64.onnx
+
