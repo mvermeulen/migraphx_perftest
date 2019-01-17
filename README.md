cf1f4c26866083fb3aca31055612d2469d89877e
Mike Vermeulen <mike@vermeulen.com>
2019-01-17 10:26:40 -0600

    Add initial file structure.

---
+++ README.md
@@ -0,0 +1,4 @@
+Repository for running performance tests on MIGraphX
+
+run.sh is the default run script.  Options are listed as comments in the
+script.
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# run.sh - script for running basic MIGraphX performance tests.
+#
+# Parameters - these can be modified by passing in as ENVIRONMENT variables.
+#              Default values are found to right of the := variable.
+
