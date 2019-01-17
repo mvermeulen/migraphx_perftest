416e816d9fc1ab73979b91badb0ee71626deb9d3
Mike Vermeulen <mike@vermeulen.com>
2019-01-17 11:10:26 -0600

    Update test results.

--- run.sh
+++ run.sh
@@ -4,6 +4,28 @@
 #
 # Parameters - these can be modified by passing in as ENVIRONMENT variables.
 #              Default values are found to right of the := variable.
+PERF_ONNX=${PERF_ONNX:="./bin/perf_onnx"}
+MIGRAPHX_LIBS=${MIGRAPHX_LIBS:="./lib"}
+ONNX_DIR=${ONNX_DIR:="./onnx"}
+OUTPUT_DIR=${OUTPUT_DIR:="./output"}
+TESTLIST=${TESTLIST:=`ls onnx`}
+TIMESTAMP=${TIMESTAMP:=`date '+%Y-%m-%d-%H:%M:%S'`}
 
-env LD_LIBRARY_PATH=./lib ./bin/perf_onnx ./onnx/resnet50i64.onnx
+export LD_LIBRARY_PATH=${MIGRAPHX_LIBS}
+
+for test in ${TESTLIST}
+do
+    testname=`basename $test .onnx`
+    testout=${OUTPUT_DIR}/$testname-${TIMESTAMP}.out
+    testerr=${OUTPUT_DIR}/$testname-${TIMESTAMP}.err
+
+    echo $TIMESTAMP " running " $testname
+    ${PERF_ONNX} ${ONNX_DIR}/$test 1>$testout 2>$testerr
+    if grep "Total timex" $testout > result; then
+	echo PASS `cat result`
+    else
+	echo FAIL `cat $testerr`
+    fi
+done
+
 
