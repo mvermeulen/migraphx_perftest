1601133ca0a1afcb94cfa2682986761c4f3d6e3f
Mike Vermeulen <mike@vermeulen.com>
2019-01-31 17:14:42 -0600

    Update the build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
 # build.sh - script for building MIGraphX performance tests.
-
+OUTPUT_DIR=${OUTPUT_DIR:=`pwd`/output}
+TIMESTAMP=${TIMESTAMP:=`date '+%Y-%m-%d-%H:%M:%S'`}
 
 # The following steps are done only once.
 if [ ! -d AMDMIGraphX ]; then
@@ -11,15 +12,29 @@ fi
 which rbuild
 result=$?
 if [ $result != 0 ]; then
-    pip install https://github.com/RadeonOpenCompute/rbuild/archive/master.tar.gz
+    pip install https://github.com/RadeonOpenCompute/rbuild/archive/master.tar.gz > $OUTPUT_DIR/install-rbuild.${TIMESTAMP}.txt 2>&1
 fi
 
 # Update the sources to the latest
 cd AMDMIGraphX
-git pull
+echo "Updating sources (see $OUTPUT_DIR/git-pull.${TIMESTAMP}.txt)"
+git pull > $OUTPUT_DIR/git-pull.${TIMESTAMP}.txt 2>&1
 
 # Build new copies
-rbuild build -d depend
+echo "Building MIGraphX (see $OUTPUT_DIR/rbuild.${TIMESTAMP}.txt)"
+rbuild build -d depend > $OUTPUT_DIR/rbuild.${TIMESTAMP}.txt 2>&1
 
 # Run the tests to make sure all is OK
-cd build && make check
+echo "Checking MIGraphX (see $OUTPUT_DIR/check.${TIMESTAMP}.txt)"
+cd build && make check >$OUTPUT_DIR/check.${TIMESTAMP}.txt 2>&1
+cd ../..
+
+if grep "100% tests passed" $OUTPUT_DIR/check.${TIMESTAMP}; then
+    cp AMDMIGraphX/build/src/*.so lib
+    cp AMDMIGraphX/build/src/onnx/*.so lib    
+    cp AMDMIGraphX/build/src/targets/*/*.so lib    
+    cp AMDMIGraphX/build/src/onnx/perf_onnx bin
+else
+    echo "FAIL - build and test run failed"
+fi
+
